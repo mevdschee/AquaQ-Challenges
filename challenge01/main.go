544fb7be2db854878167bef06a2fdba9de87e73e
Maurits van der Schee <maurits@vdschee.nl>
2018-12-28 21:24:45 +0100

    challenge01

--- challenge01/main.go
+++ challenge01/main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"io/ioutil"
 	"regexp"
+	"strings"
 )
 
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
 	line := strings.TrimSpace(string(input))
-    re := regexp.MustCompile(`[^0-9a-f]`)
-    line = re.ReplaceAllString(line, `0`)
-	for ;len(line)%3!=0; {
+	re := regexp.MustCompile(`[^0-9a-f]`)
+	line = re.ReplaceAllString(line, `0`)
+	for len(line)%3 != 0 {
 		line += string('0')
 	}
-	answer:=""
-	for i:=0;i<len(line);i+=len(line)/3 {
-		answer += line[i:i+2]
+	answer := ""
+	for i := 0; i < len(line); i += len(line) / 3 {
+		answer += line[i : i+2]
 	}
 	fmt.Println(answer)
 }
