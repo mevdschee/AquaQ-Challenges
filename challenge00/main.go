574482cefeef36379e9afc0daefdf8a2d6ab03af
Maurits van der Schee <maurits@vdschee.nl>
2018-12-28 21:24:58 +0100

    challenge00

--- challenge00/main.go
+++ challenge00/main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strconv"
 	"strings"
-	"io/ioutil"
 )
 
 func main() {
 	file, _ := ioutil.ReadFile("input.txt")
 	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
 	answer := ""
-	letters := []string{" ","","abc","def","ghi","jkl","mno","pqrs","tuv", "wxyz"}
+	letters := []string{" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
 	for _, line := range lines {
 		parts := strings.Split(line, " ")
-		number,_ := strconv.Atoi(parts[0])
-		count,_ := strconv.Atoi(parts[1])
+		number, _ := strconv.Atoi(parts[0])
+		count, _ := strconv.Atoi(parts[1])
 		len := len(letters[number])
-		answer+=string(letters[number][(count-1)%len])
+		answer += string(letters[number][(count-1)%len])
 	}
 	fmt.Println(answer)
 }
