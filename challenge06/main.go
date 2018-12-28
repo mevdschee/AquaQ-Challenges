ff67fd9996e41ce22f3dd86430ff300fe78e2f30
Maurits van der Schee <maurits@vdschee.nl>
2018-12-28 22:43:42 +0100

    challenge06

---
+++ challenge06/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func adders(result string, left, parts int) int {
+	if parts == 1 {
+		result += strconv.Itoa(left) + " "
+		sum := 0
+		for _, c := range result {
+			if c == '1' {
+				sum += 1
+			}
+		}
+		//fmt.Println(result)
+		return sum
+	}
+	sum := 0
+	for i := 0; i <= left; i++ {
+		sum += adders(result+strconv.Itoa(i)+" ", left-i, parts-1)
+	}
+	return sum
+}
+
+func main() {
+	input, _ := ioutil.ReadFile("input.txt")
+	line := strings.Split(strings.TrimSpace(string(input)), " numbers which sum to ")
+	parts, _ := strconv.Atoi(line[0])
+	target, _ := strconv.Atoi(line[1])
+	fmt.Println(adders("", target, parts))
+}
