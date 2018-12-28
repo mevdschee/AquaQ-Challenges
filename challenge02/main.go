0d7c35a92c639308401b7e89e1ea27052c74e372
Maurits van der Schee <maurits@vdschee.nl>
2018-12-28 21:24:34 +0100

    challenge02

--- challenge02/main.go
+++ challenge02/main.go
@@ -2,39 +2,39 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"io/ioutil"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
 	line := strings.TrimSpace(string(input))
-	numberStrings := strings.Split(line," ")
+	numberStrings := strings.Split(line, " ")
 	numbers := []int{}
-	for _,str := range numberStrings {
+	for _, str := range numberStrings {
 		number, _ := strconv.Atoi(str)
 		numbers = append(numbers, number)
 	}
 	for {
 		memory := map[int]int{}
 		var next []int
-		for i,number := range numbers {
+		for i, number := range numbers {
 			pos, found := memory[number]
 			if found {
-				next = append(numbers[:pos],numbers[i:]...)
+				next = append(numbers[:pos], numbers[i:]...)
 				break
 			}
 			memory[number] = i
 		}
-		if next==nil {
+		if next == nil {
 			break
 		}
 		numbers = next
 	}
 	sum := 0
-	for _,number := range numbers {
-		sum+=number
+	for _, number := range numbers {
+		sum += number
 	}
 	fmt.Println(sum)
 }
