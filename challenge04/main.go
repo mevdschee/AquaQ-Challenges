c3d0f75ea9fddf4bfc7eec0da7a38c5693cab3f7
Maurits van der Schee <maurits@vdschee.nl>
2018-12-28 21:43:51 +0100

    challenge04

---
+++ challenge04/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func gcd(a, b int) int {
+	for b != 0 {
+			t := b
+			b = a % b
+			a = t
+	}
+	return a
+}
+
+func main() {
+	input, _ := ioutil.ReadFile("input.txt")
+	prime, _ := strconv.Atoi(strings.TrimSpace(string(input)))
+	sum := 0
+	for i := 1; i < prime; i++ {
+		if gcd(prime, i) == 1 {
+			sum += i
+		}
+	}
+	fmt.Println(sum)
+}
