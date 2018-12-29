11d95b6229325e7455c0fd7e9c13ff5818eee61c
Maurits van der Schee <maurits@vdschee.nl>
2018-12-29 22:34:41 +0100

    challenge08

--- challenge08/main.go
+++ challenge08/main.go
@@ -14,9 +14,9 @@ func main() {
 	freshCereal := 0
 	freshMilk := 0
 	for i, line := range lines[1:] {
-		for j:=-5;j<0;j++ {
-			if i+j>=0 && boughtMilk[i+j]>=100 {
-				if freshCereal>=100 {
+		for j := -5; j < 0; j++ {
+			if i+j >= 0 && boughtMilk[i+j] >= 100 {
+				if freshCereal >= 100 {
 					freshCereal -= 100
 					boughtMilk[i+j] -= 100
 					break
@@ -29,9 +29,9 @@ func main() {
 		boughtMilk = append(boughtMilk, milk)
 		freshCereal += cereal
 		freshMilk = 0
-		for j:=-4;j<=0;j++ {
-			if i+j>=0 {
-				freshMilk+=boughtMilk[i+j]
+		for j := -4; j <= 0; j++ {
+			if i+j >= 0 {
+				freshMilk += boughtMilk[i+j]
 			}
 		}
 		//fmt.Printf("%d: %d %d\n",i+1,freshMilk,freshCereal)
