9a1a5b28951d8befcc9fd746a0b56dac410344dd
Maurits van der Schee <maurits@vdschee.nl>
2019-01-03 01:06:38 +0100

    challenge19

---
+++ challenge19/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
+	sum := 0
+	for _, line := range lines {
+		field := map[int]bool{}
+		parts := strings.Split(line, " ")
+		steps, _ := strconv.Atoi(parts[0])
+		size, _ := strconv.Atoi(parts[1])
+		for p := 2; p < len(parts); p += 2 {
+			x, _ := strconv.Atoi(parts[p+1])
+			y, _ := strconv.Atoi(parts[p])
+			field[y*1000+x] = true
+		}
+		for i := 0; i < steps; i++ {
+			newField := map[int]bool{}
+			for p := range field {
+				for _, o := range []int{-1000, -1, 1, 1000} {
+					pos := p + o
+					y := pos / 1000
+					x := pos % 1000
+					if x < 0 || x >= size || y < 0 || y >= size {
+						continue
+					}
+					if newField[pos] {
+						delete(newField, pos)
+					} else {
+						newField[pos] = true
+					}
+				}
+			}
+			field = newField
+		}
+		sum += len(field)
+	}
+	fmt.Println(sum)
+}
