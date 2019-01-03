805ae8a0cdd0e661a826b3ce3bef3e14985b4897
Maurits van der Schee <maurits@vdschee.nl>
2019-01-03 14:46:03 +0100

    challenge19

--- challenge19/main.go
+++ challenge19/main.go
@@ -7,21 +7,6 @@ import (
 	"strings"
 )
 
-func fieldToString(field map[int]bool, size int) string {
-	str := "\033[2J\033[;H"
-	for y := 0; y < size; y++ {
-		for x := 0; x < size; x++ {
-			if field[y*1000+x] {
-				str += "#"
-			} else {
-				str += "."
-			}
-		}
-		str += "\n"
-	}
-	return str
-}
-
 func main() {
 	file, _ := ioutil.ReadFile("input.txt")
 	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
@@ -36,26 +21,73 @@ func main() {
 			y, _ := strconv.Atoi(parts[p])
 			field[y*1000+x] = true
 		}
-		for i := 0; i < steps; i++ {
-			newField := map[int]bool{}
-			for p := range field {
-				for _, o := range []int{-1000, -1, 1, 1000} {
-					pos := p + o
-					y := pos / 1000
-					x := pos % 1000
-					if x < 0 || x >= size || y < 0 || y >= size {
-						continue
-					}
-					if newField[pos] {
-						delete(newField, pos)
-					} else {
-						newField[pos] = true
-					}
+
+		emptyRow := strings.Repeat(" ", size)
+		rows := []string{emptyRow}
+		for y := 0; y < size; y++ {
+			row := ""
+			for x := 0; x < size; x++ {
+				if field[y*1000+x] {
+					row += "#"
+				} else {
+					row += "."
 				}
 			}
-			field = newField
+			rows = append(rows, row)
 		}
-		sum += len(field)
+		rows = append(rows, emptyRow)
+
+		//fmt.Println(strings.Join(rows, "\n"))
+
+		fieldSeen := map[string]int{}
+		rowSeen := map[string]string{}
+		for i := 0; i < steps; i++ {
+			str := strings.Join(rows, "")
+			j, found := fieldSeen[str]
+			if found {
+				fieldSeen = map[string]int{}
+				steps = i + (steps-j)%(i-j)
+			}
+			newRows := []string{emptyRow}
+			for y := 0; y < size; y++ {
+				source := strings.Join(rows[y:y+3], "")
+				newRow, found := rowSeen[source]
+				if !found {
+					for x := 0; x < size; x++ {
+						count := 0
+						if x > 0 {
+							if source[x+size-1] == '#' {
+								count++
+							}
+						}
+						if x < size-1 {
+							if source[x+size+1] == '#' {
+								count++
+							}
+						}
+						if source[x] == '#' {
+							count++
+						}
+						if source[x+2*size] == '#' {
+							count++
+						}
+						if count%2 == 1 {
+							newRow += "#"
+						} else {
+							newRow += "."
+						}
+					}
+					rowSeen[source] = newRow
+				}
+				newRows = append(newRows, newRow)
+			}
+			newRows = append(newRows, emptyRow)
+			fieldSeen[str] = i
+
+			rows = newRows
+			//fmt.Println(strings.Join(rows, "\n"))
+		}
+		sum += strings.Count(strings.Join(rows, "\n"), "#")
 	}
 	fmt.Println(sum)
 }
